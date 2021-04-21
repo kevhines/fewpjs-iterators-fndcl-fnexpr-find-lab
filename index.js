db8a118ac003f249383409de5a96e400e138153d
Kevin <kevhines@gmail.com>
2021-04-21 15:47:16 -0400

    commit tree f39cf121f84c4d1d815e845998b68ed5717199ed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+function superbowlWin(records) {
+    const winningYear = records.find( function(s) { return s.result === "W"})
+    if (winningYear) {
+        return winningYear.year
+    } else {
+            return undefined
+    }
+    
+
+}
